05ce1e7efe1782dc0d542c4d01f920da1f49ead7
John Eriksson <wmjoers@gmail.com>
2024-02-13 10:15:21 +0100

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MapComponent } from './map/map.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'map', component: MapComponent }, // Route for "/map"
+  { path: '', redirectTo: '/map', pathMatch: 'full' } // Redirect from "/" to "/map"
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
